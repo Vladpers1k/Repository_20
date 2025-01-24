561c3237fa3a309f4b28f819c5c072f1c954773b
Vladpers1k <vladik340123@gmail.com>
2025-01-24 12:16:35 +0200

    created form

---
+++ src/components/form.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+
+const MyForm = () => {
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Ім'я є обов'язковим").min(2, "Ім'я має містити щонайменше 2 символи"),
+    email: Yup.string().email('Невірний формат електронної пошти').required("Електронна пошта є обов'язковою"),
+    password: Yup.string().required("Пароль є обов'язковим").min(6, 'Пароль має містити щонайменше 6 символів')
+  })
+
+  const initialValues = {
+    name: '',
+    email: '',
+    password: ''
+  }
+
+  const handleSubmit = (values) => {
+    console.log('Дані форми:', values)
+    alert('Форма успішно відправлена!')
+  }
+
+  return (
+    <div style={{ maxWidth: '400px', margin: '0 auto' }}>
+      <h2>Реєстраційна форма</h2>
+      <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+        {({ isSubmitting }) => (
+          <Form>
+            {}
+            <div>
+              <label htmlFor="name">Ім'я:</label>
+              <Field type="text" name="name" placeholder="Введіть ім'я" id="name" />
+              <ErrorMessage name="name" component="div" style={{ color: 'red', fontSize: '14px' }} />
+            </div>
+
+            {}
+            <div>
+              <label htmlFor="email">Електронна пошта:</label>
+              <Field type="email" name="email" placeholder="Введіть email" id="email" />
+              <ErrorMessage name="email" component="div" style={{ color: 'red', fontSize: '14px' }} />
+            </div>
+
+            {}
+            <div>
+              <label htmlFor="password">Пароль:</label>
+              <Field type="password" name="password" placeholder="Введіть пароль" id="password" />
+              <ErrorMessage name="password" component="div" style={{ color: 'red', fontSize: '14px' }} />
+            </div>
+
+            {}
+            <button type="submit" disabled={isSubmitting} style={{ marginTop: '10px' }}>
+              Відправити
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  )
+}
+
+export default MyForm
